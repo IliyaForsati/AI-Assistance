be116c20158d79d9af9afc2289bf59947b0b455f
Iliya Forsati <iliyaforsati2005@gmail.com>
2025-08-20 13:10:06 +0330

    dockerize projecr

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,18 @@ RUN apt-get update
 RUN apt-get install -y python3
 RUN apt-get install -y python3-pip
 
-WORKDIR /root
+# for when running -it
+RUN apt-get install -y vim
 
-COPY ./ /root/
+WORKDIR /home/app
+
+COPY ./requirements.txt /home/app/
 RUN pip install -r ./requirements.txt
 
 EXPOSE 8000
 
-CMD ["fastapi", "dev", "main.py"]
-# CMD ["python3", "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["fastapi", "run", "main.py"]
+
+# you can say "dokcer run -p (port):8000 -it <image> /bin/bash" 
+# to run command CMD [ "/bin/bash" ]
+# or any ubuntu command
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request # pyright: ignore[reportMissingImports]
+from fastapi.responses import HTMLResponse, JSONResponse # pyright: ignore[reportMissingImports]
+from fastapi.templating import Jinja2Templates # pyright: ignore[reportMissingImports]
+from pydantic import BaseModel # pyright: ignore[reportMissingImports]
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def root():
-    return "hello world!"
+class Message(BaseModel):
+    message: str
+
+@app.get("/", response_class=HTMLResponse)
+def get_chat(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/chat")
+def chat(msg: Message):
+    user_msg = msg.message
+    reply = f"{user_msg}"
+    return JSONResponse({"reply": reply})
